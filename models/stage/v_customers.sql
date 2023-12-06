553ab094f2834d3a655db56fa904126ccedd445e
pooja s <shendripooja@gmail.com>
2023-12-06 12:53:14 +0000

    stg_customer_created

---
+++ models/stage/v_customers.sql
@@ -0,0 +1,18 @@
+{{
+        config(
+                materialized='view'
+        )
+}}
+
+select
+        c_custkey as customer_id,
+        c_nationkey as nation_id,
+        c_name as name,
+        n_name as nation,
+        c_address as address,
+        c_phone as phone_number,
+        c_acctbal as account_balance,
+        c_mktsegment as market_segment,
+        c_comment as comment
+from {{source('src','customers')}} as c
+join {{source('src','nations')}} as n on c.c_nationkey = n.n_nationkey
